cfd9035cffeebb3cc8d83408fbda4c52867c1057
ukitahide <yukihunji@gmail.com>
2026-05-11 20:32:18 +0900

    Setup API routing for notes and users

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -23,4 +23,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('notes.urls')),
+    path('api/', include('users.urls')),
+
 ]
