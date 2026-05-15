b5f54a0a960df774cb660ef7b63c93d458901148
ukitahide <yukihunji@gmail.com>
2026-05-15 19:38:51 +0900

    add NotesPage.tsx

--- frontend/src/features/notes/pages/NotesPage.tsx
+++ frontend/src/features/notes/pages/NotesPage.tsx
@@ -1,9 +1,47 @@
+import { useEffect, useState } from "react";
+import { getNotes } from "../api/noteApi";
+
+
+
+type Note = {
+        id: number,
+        title: string,
+        content: string,
+    };
 
 
 export default function NotesPage() {
+
+    const [notes, setNotes] = useState<Note[]>([]);
+
+    useEffect(() => {
+        const fetchNotes = async () => {
+
+            try {
+                const data = await getNotes();
+                setNotes(data);
+            } catch (error) {
+                console.error("ノート取得に失敗");
+            }
+
+
+        }
+
+        fetchNotes();
+
+    }, [])
+
+
+
     return (
         <div>
-            ノート一覧
+            {notes.map((note) => (
+                <div key={note.id}>
+                    <h3>{note.title}</h3>
+                    <p>{note.content}</p>
+                </div>
+            ))}
         </div>
     )
+
 }
