48c4ffb06ce8be7388179e0285dfaf8a862d5df9
ukitahide <yukihunji@gmail.com>
2026-05-21 14:42:54 +0900

    create restoreNoteAPI & deleteNoteForeverAPI

--- frontend/src/features/notes/api/noteApi.tsx
+++ frontend/src/features/notes/api/noteApi.tsx
@@ -88,7 +88,7 @@ export const updateNote = async(id: number, title: string, content: string) => {
 
 
 
-export const moveToTrash = async(id: Number) => {
+export const moveToTrash = async(id: number) => {
     const token = localStorage.getItem("access");
 
     const res = await api.patch(
@@ -123,3 +123,47 @@ export const getTrashNotes = async () => {
 
     return res.data;
 }
+
+
+
+
+// ゴミ箱に入れたノートを復元する
+export const restoreNote = async (id: number) => {
+    const token = localStorage.getItem("access");
+    const res = await api.patch(
+        `/notes/${id}`,
+        {
+            is_deleted: false
+        },
+        {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+    );
+
+    return res.data;
+
+}
+
+
+
+
+
+
+// ノートを完全に削除する
+export const deleteNoteForever = async (id: number) => {
+    const token = localStorage.getItem("access");
+
+    await api.delete(
+        `/notes/${id}`,
+        {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+    );
+}
+
+
+
