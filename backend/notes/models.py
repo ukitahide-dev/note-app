9debaf1bee780d6d420aad209797cb3208d89cf1
ukitahide <yukihunji@gmail.com>
2026-05-23 12:22:08 +0900

    create Label table

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -4,13 +4,24 @@ from django.db import models
 from users.models import User
 
 
+
+class Label(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
+
 class Note(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
     title = models.CharField(max_length=100)
     content = models.TextField()
+    labels = models.ManyToManyField(Label, blank=True)
     is_deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
 
 
 
