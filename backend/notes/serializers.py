a5e2fd67b48761817378658399be0f6395289c4e
ukitahide <yukihunji@gmail.com>
2026-05-23 12:31:14 +0900

    create LabelSerializer

--- backend/notes/serializers.py
+++ backend/notes/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
-from .models import Note
+from .models import Note, Label
+
+
+
+
+class LabelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Label
+        fields = '__all__'
+
 
 
 class NoteSerializer(serializers.ModelSerializer):
+    labels = LabelSerializer(many=True)  # Labelをネストして返す。1つのNoteにLabelは複数あるから。
+
     class Meta:
         model = Note
         fields = '__all__'
