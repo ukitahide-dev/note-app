9e4120ebb8f9d537d699fda45b579d9f3294c2e0
ukitahide <yukihunji@gmail.com>
2026-05-25 09:57:21 +0900

    add labels props in Sidebar

--- frontend/src/shared/components/Sidebar/Sidebar.tsx
+++ frontend/src/shared/components/Sidebar/Sidebar.tsx
@@ -6,13 +6,25 @@ import styles from "./Sidebar.module.css";
 
 
 
+
+type Label = {
+    id: number;
+    name: string
+}
+
 type Props = {
+    labels: Label[];
     isOpen: boolean;
+
 };
 
 
 
-export default function Sidebar({ isOpen }: Props) {
+
+
+
+
+export default function Sidebar({ labels, isOpen }: Props) {
     const navigate = useNavigate();
 
     return (
@@ -33,7 +45,23 @@ export default function Sidebar({ isOpen }: Props) {
                 )}
             </div>
 
-            
+            {labels.map((label) => (
+
+                <div key={label.id}
+                     className={styles.item}
+                     onClick={() => navigate(`/labels/${label.name}`)}
+                >
+
+                    {isOpen && (
+                        <span>{label.name}</span>
+                    )}
+
+                </div>
+
+
+            ))}
+
+
 
             <div className={styles.item} onClick={() => navigate("/notes/trash")}>
                 🗑
