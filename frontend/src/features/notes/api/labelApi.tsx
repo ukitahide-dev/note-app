37f3aeecf59bb6b73ddd799e79966341e13cc041
ukitahide <yukihunji@gmail.com>
2026-05-31 14:37:06 +0900

    create deleteLabelAPI and updateLabelAPI

--- frontend/src/features/notes/api/labelApi.tsx
+++ frontend/src/features/notes/api/labelApi.tsx
@@ -37,3 +37,51 @@ export const createLabel = async (name: string) => {
 
     return res.data;
 }
+
+
+
+
+// ラベルを編集する
+export const updateLabel = async (
+    id: number,
+    name: string
+) => {
+    const token = localStorage.getItem("access");
+
+    const res = await api.patch(
+        `/labels/${id}/`,
+        {
+            name,
+        },
+        {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+    );
+
+    return res.data;
+};
+
+
+
+// ラベルを削除する
+export const deleteLabel = async (
+    id: number
+) => {
+
+    const token = localStorage.getItem("access");
+
+    await api.delete(
+        `/labels/${id}`,
+
+        {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+    );
+
+}
+
+
